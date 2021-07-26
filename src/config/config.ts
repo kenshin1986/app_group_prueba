4d45c3e85dd644579516de257cb3a81b19d03972
kenshin1986 <40031773+kenshin1986@users.noreply.github.com>
2021-07-26 08:44:33 -0500

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export default{
-    API_URL: process.env.API_URL || 'http://apirestonegroup.herokuapp.com',
+    API_URL: process.env.API_URL || 'https://apirestonegroup.herokuapp.com',
     API_CDN: process.env.API_CDN || 'https://apirestonegroup.herokuapp.com',
 }
